cf2c1b77fa63473aa572d33820eda3ce82540b27
dmlindsa <dmlindsa@go.olemiss.edu>
2022-03-20 00:39:56 -0500

    updated some origin settings

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 var corsOptions = {
-  origin: "http://localhost:8081"
+  origin: "http://172.17.0.4:8081"
 };
 app.use(cors(corsOptions));
 const db = require("./app/models");
